16b59c329c67a7abc7474642127e384697f69318
Jasper John Lepardo <jlepardo@kmc.int>
2017-05-29 21:19:16 +0800

    Sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,14 @@
 $('.form-group').on( 'change keyup keydown paste cut', 'textarea', function (){
     $(this).height(0).height(this.scrollHeight);
 }).find( 'textarea' ).change();
+
+
+
+// TABLE TOGGLE
+
+$(document).ready(function() {
+	$('.table-togglable tbody tr').click(function(){
+		var aHref = $(this).find('a').attr('href');
+		window.open(aHref, '_blank');
+	});
+});
