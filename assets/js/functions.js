be41ccc5b6d720171a5781613566c0b35b4d3861
JSPR Design <jspr.dsgn@gmail.com>
2017-07-11 12:05:21 +0800

    Sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,8 @@
 // Auto resize textarea
 
-$('.form-group').on( 'change keyup keydown paste cut', 'textarea', function (){
-    $(this).height(0).height(this.scrollHeight);
-}).find( 'textarea' ).change();
+// $('.form-group').on( 'change keyup keydown paste cut', 'textarea', function (){
+//     $(this).height(0).height(this.scrollHeight);
+// }).find( 'textarea' ).change();
 
 
 
