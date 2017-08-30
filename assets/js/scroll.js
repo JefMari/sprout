2ecfa198be21c584e2d886e53523acc78c253c98
JSPR Design <jspr.dsgn@gmail.com>
2017-08-30 12:39:32 +0800

    Sync

---
+++ assets/js/scroll.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('#sub-navigation').removeClass('fixed-down').addClass('fixed-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('#sub-navigation').removeClass('fixed-up').addClass('fixed-down');
+          }
+      }
+
+      lastScrollTop = st;
+  }
+});
